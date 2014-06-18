e830940ec5b9c37f1200dd52cf33fb643767e750
João Vieira <joao@evrythng.com>
2014-06-18 11:22:04 +0100

    Generated documentation for 69d2b90bffb016ca9c348df7dc0bbc04ba7371cd

--- assets/behavior.js
+++ assets/behavior.js
@@ -384,7 +384,17 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                 "title": "property"
               },
               "depth": 3,
-              "outline": []
+              "outline": [
+                {
+                  "type": "heading",
+                  "data": {
+                    "level": 2,
+                    "title": "PROPERTY.JS",
+                    "slug": "propertyjs"
+                  },
+                  "depth": 2
+                }
+              ]
             }, {
               "type": "file",
               "data": {
